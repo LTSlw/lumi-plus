9911ecf55612497450368a64d176cf00deba82c9
LTSlw <38352100+lw-tech-soft@users.noreply.github.com>
2020-09-04 20:18:43 +0800

    20200830&20200904
    重新打包model.dd，说明文件将尽快更新

---
+++ LTSlw/scpieces/time1970/time1970.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <ctime>
+#include <sys/timeb.h>
+using namespace std;
+
+int main()
+{
+	cout << time(NULL) << endl;//s
+	timeb t;
+	ftime(&t);
+	cout << t.time << endl;
+	cout << t.millitm << endl;
+	return 0;
+}
