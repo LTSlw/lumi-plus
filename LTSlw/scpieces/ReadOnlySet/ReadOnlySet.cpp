876d81275652323dae0469767d490f114df672d5
LTSlw <38352100+lw-tech-soft@users.noreply.github.com>
2020-09-21 19:32:24 +0800

    ReadOnlySet
    code piece ReadOnlySet

---
+++ LTSlw/scpieces/ReadOnlySet/ReadOnlySet.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <locale>
+#include <Windows.h>
+#include <tchar.h>
+using namespace std;
+
+int _tmain(int argc, wchar_t* argv[])
+{
+	setlocale(LC_ALL, "chs");
+	if (argc != 2)
+		return -1;
+	unsigned long ac = 0; //File Attribute Constants
+	ac = GetFileAttributes(argv[1]);
+	if (ac == INVALID_FILE_ATTRIBUTES)
+	{
+		wcout << L"Fail to get access value" << endl;
+		return 1;
+	}
+	wcout << L"ACCESS VALUE: " << ac << endl;
+	if (SetFileAttributes(argv[1], 1))
+		wcout << L"Read Only Set Successfully." << endl;
+	else
+		wcout << "Failed with code: " << GetLastError() << endl;
+	return 0;
+}
